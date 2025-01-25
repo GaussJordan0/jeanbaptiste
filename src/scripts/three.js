a9d1a2820e16026dec4d39d05f8d8c34e6d2797c
GaussJordan0 <gaussjordanwork@gmail.com>
2025-01-25 21:51:35 +0200

    Please

--- src/scripts/three.js
+++ src/scripts/three.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
-import vertexShader from "./shaders/vertexshader.glsl";
-import fragmentShader from"./shaders/fragmentshader.glsl";
+import vertexShader from "./shaders/vertexshader.glsl?raw";
+import fragmentShader from "./shaders/fragmentshader.glsl";
 import { animate } from "motion";
 
 window.addEventListener("DOMContentLoaded", () => {
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,30 @@
-// vite.config.js
 import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+
   plugins: [glsl()],
-  build: {
-    // Required for Vercel deployment
-    rollupOptions: {
-      external: ['three', 'motion'] // Add any other external deps here
-    }
-  },
-  resolve: {
-    // Explicitly list extensions Vite should resolve
-    extensions: ['.js', '.glsl']
-  },
-  assetsInclude: ['**/*.glsl']
 });
+
+
+// assetsInclude: ['**/*.glsl']
+
+// vite.config.js
+// import glsl from 'vite-plugin-glsl';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   plugins: [glsl()],
+//   assetsInclude: ['**/*.glsl'],
+//   build: {
+//     // Required for Vercel deployment
+//     rollupOptions: {
+//       external: ['three', 'motion'] // Add any other external deps here
+//     }
+//   },
+//   resolve: {
+//     // Explicitly list extensions Vite should resolve
+//     extensions: ['.js', '.glsl']
+//   },
+
+// });

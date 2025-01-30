d1d21c13a18c5f2574d772b180cbb8f572dd5b57
GaussJordan0 <gaussjordanwork@gmail.com>
2025-01-30 12:12:20 +0200

    maybe a mistake?

--- src/main.js
+++ src/main.js
@@ -4,10 +4,20 @@ import "./scripts/mouse";
 import "./scripts/iframes";
 import "./scripts/animations";
 
-// import { animate, scroll } from "motion";
+import { animate } from "motion";
 import Lenis from "lenis";
 
 const lenis = new Lenis({
   autoRaf: true,
   lerp: 0.05,
 });
+
+// const body = document.querySelector("body");
+// const trigger = document.getElementById("trigger");
+// const observer = new IntersectionObserver((entries)=>{
+
+//  body.style.transition = "background-color 1s";
+// body.style.background = entries[0].isIntersecting ? "white" : "#090979";
+// body.style.color = entries[0].isIntersecting ? "#090979" : "white";
+// }, {threshold: 0.4,})
+// observer.observe(trigger)
